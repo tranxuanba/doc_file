b22688cdd8e61625aa57ee6949f8733855ed4ebb
ishopjapan <thoitran2107@gmail.com>
2022-07-24 16:05:54 +0700

    bai tap thuc hanh 1 doc file

---
+++ src/ReadFileExample.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class ReadFileExample {
+    public void readFileText(String filePath) throws IOException {
+        try {
+            //doc file theo duong dan
+            File file = new File(filePath);
+            // kiem tra neu file ko ton tai thi in ra ngoai le
+            if (!file.exists()) {
+                throw new FileNotFoundException();
+            }
+            //doc tung dong cua file va tien hanh cong tong lai
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line = "";
+            int sum = 0;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+                sum += Integer.parseInt(line);
+            }
+            br.close();
+
+            // hien thi ra man hinh tong cac so nguyen trong file
+            System.out.println("tong = " + sum);
+        }catch (Exception e) {
+            // truong hop file khong ton tai hoac noi dung file co loi thi hien thi :
+            System.out.println("file khong ton tai hoac noi dung file co loi");
+
+        }
+
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("nhap vao duong dan file:");
+        Scanner sc = new Scanner(System.in);
+        String path = sc.nextLine();
+        ReadFileExample readFileExample = new ReadFileExample();
+        readFileExample.readFileText(path);
+    }
+}
---
+++ src/TestFileClass.java
@@ -0,0 +1,29 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+
+public class TestFileClass {
+    public static void main(String[] args) throws IOException {
+       // File file = new File("image.us/gif");
+//        System.out.println("ggagagffdg" + file.exists());
+//        System.out.println(file.length());
+//        System.out.println(file.canRead());
+//        System.out.println("Can it be written? " + file.canWrite());
+//
+//         System.out.println("Is it a directory? " + file.isDirectory());
+//
+//         System.out.println("Is it a file? " + file.isFile());
+//
+//         System.out.println("Is it absolute? " + file.isAbsolute());
+//
+//         System.out.println("Is it hidden? " + file.isHidden());
+//
+//         System.out.println("Absolute path is " + file.getAbsolutePath());
+//
+//        System.out.println("Last modified on " + new Date(file.lastModified()));
+       // System.err.println("dsfgfdg");
+        int i=System.in.read();//tra ve ma ASCII cua ky tu dau tie
+        System.out.println((char)i);//in ky tu lay duoc ra man hinh
+
+    }
+}
